d634797966a6cbc4a21cd6445d41dd51af2c13b3
Thadeu José <thadeujs@gmail.com>
2021-02-13 20:55:03 -0300

    Create get_all_files
    Function who return all files of a folder

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from PIL import Image
+from os import listdir
+from os.path import isfile, join
 
 def convert_webp_to_png(inputpath, outputpath, file_extension):
     im = Image.open(inputpath).convert("RGB")
     im.save(outputpath, file_extension)
+
+def get_all_files(path):
+    return [f for f in listdir(path) if isfile(join(path, f))]
 
 inputpath = "input/1 - The Flight.webp"
 outputpath = "output/1 - The Flight.png"
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import os
-from main import convert_webp_to_png
+from main import convert_webp_to_png, get_all_files
 
 def test_converter():
     inputpath = "input_test/testimage.webp"
@@ -9,3 +9,17 @@ def test_converter():
         os.remove(outputpath)
     convert_webp_to_png(inputpath, outputpath,file_extension)
     assert os.stat(outputpath).st_size > os.stat(inputpath).st_size
+
+def test_get_all_files():
+    files = ["testimage.webp","testimage2.webp",
+             "testimage3.webp","testimage4.webp",
+             "testimage5.webp","testimage6.webp",
+             "testimage7.webp"
+            ]
+    all_files = get_all_files('input_test')
+    for i in range(len(all_files)):
+        if files[i] != all_files[i]:
+            print(files[i])
+            print(all_files[i])
+            assert False
+    assert True
